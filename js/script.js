5f72d6b73a22f1ead27c206f00e6f2c82a84f6e2
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-11-19 20:21:48 +0530

    Adding feature of fetching coords of user

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
 jQuery(document).ready(function($) {
 	console.log('DOM loaded!');
 	
+	var userLocationDiv = document.getElementById("userLocation");
+
+	function getLocation() {
+	console.log('in getLocation!');
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition, showError);
+		} else { 
+			userLocationDiv.innerHTML = "Geolocation is not supported by this browser.";
+		}
+	}
+
+	function showPosition(position) {
+		userLocationDiv.innerHTML = "Latitude: " + position.coords.latitude + 
+		"<br>Longitude: " + position.coords.longitude;
+	}
+
+	function showError(error) {
+		switch(error.code) {
+			case error.PERMISSION_DENIED:
+			userLocationDiv.innerHTML = "User denied the request for Geolocation."
+			break;
+			case error.POSITION_UNAVAILABLE:
+			userLocationDiv.innerHTML = "Location information is unavailable."
+			break;
+			case error.TIMEOUT:
+			userLocationDiv.innerHTML = "The request to get user location timed out."
+			break;
+			case error.UNKNOWN_ERROR:
+			userLocationDiv.innerHTML = "An unknown error occurred."
+			break;
+		}
+	}
+
+	getLocation();
+
+
 });
